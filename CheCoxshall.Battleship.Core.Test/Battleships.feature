be35ac6a3fe24a86a444eca891a35cd2d6c31fb2
Che Coxshall <Che.Coxshall@berryworld.com>
2017-07-25 14:46:57 +0100

    Lots of shiny SpecFlow type testings

---
+++ CheCoxshall.Battleship.Core.Test/Battleships.feature
@@ -0,0 +1,66 @@
+﻿Feature: Battleships
+	Make sure the game board is correctly populated before starting a game
+
+@boardSetup
+Scenario: Ensure Readiness
+	Given I have placed an aircraftcarrier horizontally at cell A1
+	And I have placed a battleship vertically at cell A2
+	And I have placed a cruiser vertically at cell B2
+	And I have placed a destroyer vertically at cell C2
+	And I have placed a submarine vertically at cell D2
+	Then the game board should be ready
+
+@boardSetup
+Scenario: Ensure I cannot start a game without placing all of my ships
+	Given I have placed an aircraftcarrier horizontally at cell A1
+	And I have placed a battleship vertically at cell A2
+	And I have placed a cruiser vertically at cell B2
+	And I have placed a destroyer vertically at cell C2
+	Then the game board should not be ready
+
+@gameplay
+Scenario: EnsureTheDestructionOfADestroyer
+	Given I have placed an aircraftcarrier horizontally at cell A1
+	And I have placed a battleship vertically at cell A2
+	And I have placed a cruiser vertically at cell B2
+	And I have placed a destroyer vertically at cell C2
+	When A shot lands at cell C2
+	And A shot lands at cell C3
+	Then My destroyer should be sunk
+	
+@gameplay
+Scenario: EnsureTheDestructionOfACarrier
+	Given I have placed an aircraftcarrier horizontally at cell A1
+	And I have placed a battleship vertically at cell A2
+	And I have placed a cruiser vertically at cell B2
+	And I have placed a destroyer vertically at cell C2
+	When A shot lands at cell A1
+	And A shot lands at cell B1
+	And A shot lands at cell C1
+	And A shot lands at cell D1
+	And A shot lands at cell E1
+	Then My aircraftcarrier should be sunk
+
+@gameBoard
+Scenario: Ensure game board does not allow overlapping ships
+	Given I have placed the following ships
+	| Ship Type       | Orientation  | Cell |
+	| aircraftcarrier | horizontally | A1   |
+	| battleship      | vertically   | A2   |
+	| cruiser         | horizontally | C4   |
+	| submarine       | vertically   | B3   |
+	| destroyer       | horizontally | I9   |
+	Then the game board should have thrown a CannotPlaceException with the message 'Ships may not overlap.'
+	
+@gameBoard
+Scenario: Ensure game board does not allow ships to fall off the edge of the world
+	Given I have placed an aircraftcarrier horizontally at cell G1
+	Then the game board should have thrown a CannotPlaceException with the message 'Ship extends beyond board boundaries.'
+	
+@gameBoard
+Scenario: Ensure game board does not allow duplicate ships
+	Given I have placed the following ships
+	| Ship Type       | Orientation  | Cell |
+	| aircraftcarrier | horizontally | A1   |
+	| aircraftcarrier | vertically   | B1   |
+	Then the game board should have thrown a CannotPlaceException with the message 'This type of ship has already been placed.'
---
+++ CheCoxshall.Battleship.Core.Test/BattleshipsSteps.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace CheCoxshall.Battleship.Core.Test
+{
+    [Binding]
+    public class BattleshipsSteps
+    {
+        private GameBoard _gameBoard = new GameBoard();
+
+        [Given(@"I have placed(?:\san\s|\sa\s)([a-zA-Z]+) (horizontally|vertically) at cell ([A-J]\d)")]
+        public void GivenIHavePlacedAShip(ShipType shipType, string orientation, Cell cell)
+        {
+            try
+            {
+                _gameBoard.PlaceShip(shipType, cell, orientation == "horizontally");
+            }
+            catch (CannotPlaceException ex)
+            {
+                ScenarioContext.Current.Set(ex);
+            }
+        }
+
+        [Given(@"I have placed the following ships")]
+        public void GivenIHavePlacedTheFollowingShips(Table table)
+        {
+            var set = table.CreateSet<ShipPlacement>();
+            foreach (var item in set)
+            {
+                try
+                {
+                    _gameBoard.PlaceShip(item.ShipType, item.Cell, item.IsHorizontal);
+                }
+                catch(CannotPlaceException ex)
+                {
+                    ScenarioContext.Current.Set(ex);
+                }
+            }
+        }
+
+
+        [When(@"A shot lands at cell ([A-J]\d)")]
+        public void WhenAShotLandsAtCell(Cell cell)
+        {
+            ScenarioContext.Current.Set(_gameBoard.RegisterShot(cell));
+        }
+       
+
+        [Then(@"the game board should(.*) be ready")]
+        public void CheckGameBoardReadiness(string not)
+        {
+            if (!String.IsNullOrWhiteSpace(not))
+                Assert.IsFalse(_gameBoard.Ready);
+            else
+                Assert.IsTrue(_gameBoard.Ready);
+        }
+
+        [Then(@"My ([a-zA-Z]+) should be sunk")]
+        public void ThenMyShipShouldBeSunk(ShipType shipType)
+        {
+            var lastShotResult = ScenarioContext.Current.Get<ShotResult>();
+            Assert.AreEqual(shipType, lastShotResult.Ship);
+            Assert.IsTrue(lastShotResult.IsSunk);
+        }
+
+        [Then(@"the game board should have thrown a CannotPlaceException with the message '(.*)'")]
+        public void ThenTheGameBoardShouldHaveThrownACannotPlaceExceptionWithTheMessage(string exceptionMessage)
+        {
+            if (ScenarioContext.Current.TryGetValue(out CannotPlaceException exception))
+                Assert.AreEqual(exceptionMessage, exception.Message);
+            else
+                Assert.Fail("Expected exception was not found");
+        }
+
+
+
+        [StepArgumentTransformation]
+        public Cell CellTransform(string cellRef)
+        {
+            return new Cell(cellRef[0], int.Parse(cellRef[1].ToString()));
+        }
+    }
+
+    public class ShipPlacement
+    {
+        public ShipType ShipType { get; set; }
+        public bool IsHorizontal { get { return Orientation == "horizontal"; } }
+        public string Orientation { get; set; }
+        public Cell Cell { get; set; }
+    }
+}
